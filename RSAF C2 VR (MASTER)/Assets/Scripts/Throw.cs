79de20fa00511d0c2d2ed59d55412140ff2d9425
crimsoncoriander <czungheng.20@ichat.sp.edu.sg>
2023-12-13 15:28:29 +0800

    Update Throw.cs

--- RSAF C2 VR (MASTER)/Assets/Scripts/Throw.cs
+++ RSAF C2 VR (MASTER)/Assets/Scripts/Throw.cs
@@ -7,12 +7,6 @@ public class Throw : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // for finding object links
     }
 }
