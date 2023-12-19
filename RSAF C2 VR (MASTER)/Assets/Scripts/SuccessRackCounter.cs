0a3b0cfb3b47870eadd29505c284308c860e9c99
crimsoncoriander <czungheng.20@ichat.sp.edu.sg>
2023-12-19 13:59:15 +0800

    www

--- RSAF C2 VR (MASTER)/Assets/Scripts/SuccessRackCounter.cs
+++ RSAF C2 VR (MASTER)/Assets/Scripts/SuccessRackCounter.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SuccessRackCounter : MonoBehaviour
 {
+    public GameObject RackCounter;
+    public int SuccessfulRackCount = 0;
+    public int RacksToCount = 3;
+    public GameObject AllRacksDone;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,14 @@ public class SuccessRackCounter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!RackCounter.activeSelf && SuccessfulRackCount < RacksToCount)
+        {
+            SuccessfulRackCount++;
+            RackCounter.SetActive(true);
+        }
+        if (SuccessfulRackCount == RacksToCount)
+        {
+            AllRacksDone.SetActive(true);
+        }
     }
 }
