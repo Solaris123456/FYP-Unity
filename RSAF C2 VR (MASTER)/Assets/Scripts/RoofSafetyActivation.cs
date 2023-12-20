6058c505db36c085c352d57a74dce79430c10e45
crimsoncoriander <czungheng.20@ichat.sp.edu.sg>
2023-12-20 15:45:13 +0800

    scripts

---
+++ RSAF C2 VR (MASTER)/Assets/Scripts/RoofSafetyActivation.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class RoofSafetyActivation : MonoBehaviour
+{
+    public bool safety = true;
+    public bool panel = false;
+    public bool lights = false;
+    public bool errorFound = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
