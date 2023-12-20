baef668b2a3788ed8ba7e367c8537f0fd4c39f86
crimsoncoriander <czungheng.20@ichat.sp.edu.sg>
2023-12-20 16:59:23 +0800

    timer added

---
+++ RSAF C2 VR (MASTER)/Assets/EventTImer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class EventTImer : MonoBehaviour
+{
+    public GameObject TimerActivationFlag;
+    public float RecordedTime;
+    public float Timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RecordedTime = 0;
+        Timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TimerActivationFlag.activeSelf)
+        {
+            Timer += Time.deltaTime;
+        }
+        else
+        {
+            RecordedTime = Timer;
+            Timer = 0;
+        }
+    }
+}
