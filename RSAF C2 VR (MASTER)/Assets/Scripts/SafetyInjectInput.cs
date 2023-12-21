8425331623145ed7184b560f4e3572f4fdf10a85
crimsoncoriander <czungheng.20@ichat.sp.edu.sg>
2023-12-21 14:18:44 +0800

    scene logic

--- RSAF C2 VR (MASTER)/Assets/Scripts/SafetyInjectInput.cs
+++ RSAF C2 VR (MASTER)/Assets/Scripts/SafetyInjectInput.cs
@@ -1,12 +1,17 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SafetyInjectInput : MonoBehaviour
 {
+    public bool activated;
+    public GameObject CeilingBoards;
+    public GameObject Lights;
+    public GameObject FM200Trigger;
     // Start is called before the first frame update
     public void SafeInjectStart ()
     {
-
+        
     }
 }
