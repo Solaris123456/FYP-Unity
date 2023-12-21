20638ce3117d508b9f92eed33fe27432a2e220b4
AmusingCustomer <114889291+AmusingCustomer@users.noreply.github.com>
2023-12-21 19:15:25 +0800

    Game Manager edits
    Summary and Assessment scripts created

--- RSAF C2 VR (MASTER)/Assets/Scripts/GameManager.cs
+++ RSAF C2 VR (MASTER)/Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
     public User CurrentUser { get; set; }
+    public string SimulationResults { get; set; }
 
     private void Awake()
     {
