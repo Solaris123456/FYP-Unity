f019d5419e0a220142757ec160c625aeddc28c0b
crimsoncoriander <czungheng.20@ichat.sp.edu.sg>
2023-12-21 15:32:26 +0800

    Fm200

--- RSAF C2 VR (MASTER)/Assets/Scripts/SafetyInjectInput.cs
+++ RSAF C2 VR (MASTER)/Assets/Scripts/SafetyInjectInput.cs
@@ -8,10 +8,10 @@ public class SafetyInjectInput : MonoBehaviour
     public bool activated;
     public GameObject CeilingBoards;
     public GameObject Lights;
-    public GameObject FM200Trigger;
+    public Fm200PressureCheck fm200PressureCheck;
     // Start is called before the first frame update
     public void SafeInjectStart ()
     {
-        
+        fm200PressureCheck.PressureCheckBegin ();
     }
 }
