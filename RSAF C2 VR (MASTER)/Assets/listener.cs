e5762131778587474de4fb61c94e123b8b4836d9
crimsoncoriander <czungheng.20@ichat.sp.edu.sg>
2023-12-28 19:41:17 +0800

    minor changes

--- RSAF C2 VR (MASTER)/Assets/listener.cs
+++ RSAF C2 VR (MASTER)/Assets/listener.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using Valve.VR;
 
 public class listener : MonoBehaviour
 {
+    public float timeToWait;
     public AudioSource source;
     public UnityEvent EventToInvoke;
+    private float RealTime;
+    public bool done;
     // Update is called once per frame
     void Update()
     {
-        if (!source.isPlaying)
+        if (!done)
         {
-            EventToInvoke.Invoke();
+            RealTime += Time.deltaTime;
+            if (RealTime > timeToWait)
+            {
+                RealTime = timeToWait;
+                if (!source.isPlaying)
+                {
+                    EventToInvoke.Invoke();
+                    done = true;
+                    RealTime = 0;
+                }
+            }
         }
     }
 }
