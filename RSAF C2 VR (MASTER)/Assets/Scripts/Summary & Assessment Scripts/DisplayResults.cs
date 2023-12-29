64af2b337dfa74a0e8c96c1f56e689b24ee21751
AmusingCustomer <114889291+AmusingCustomer@users.noreply.github.com>
2023-12-29 11:50:06 +0800

    Update DisplayResults.cs

--- RSAF C2 VR (MASTER)/Assets/Scripts/Summary & Assessment Scripts/DisplayResults.cs
+++ RSAF C2 VR (MASTER)/Assets/Scripts/Summary & Assessment Scripts/DisplayResults.cs
@@ -43,8 +43,8 @@ public class DisplayResults : MonoBehaviour
         //lightErrorPenaltyString
 
         //TimeSpan timeSpan = new TimeSpan((long)(originalTimeTaken * TimeSpan.TicksPerSecond));
-        string ogTimeTaken = originalTimeTaken.ToString(@"mm\:ss\.fff");
-        string fiTimeTaken = finalTimeTaken.ToString(@"mm\:ss\.fff");
+        string ogTimeTaken = originalTimeTaken.ToString(); //@"mm\:ss\.fff"
+        string fiTimeTaken = finalTimeTaken.ToString(); //@"mm\:ss\.fff"
 
         originalTimeTakenText.text = "Original Time Taken: " + ogTimeTaken;
         finalTimeTakenText.text = "Final Time Taken: " + fiTimeTaken;
