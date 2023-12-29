400083dc99a78563896a2f9710d2ed78aa33aee8
crimsoncoriander <czungheng.20@ichat.sp.edu.sg>
2023-12-29 15:59:20 +0800

    flicker lights

---
+++ RSAF C2 VR (MASTER)/Assets/Flicker.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using Valve.VR;
+
+public class Flicker : MonoBehaviour
+{
+    public GameObject FlickerObject;
+    public float speedMultiplier;
+    public float timeBeforeFlicker;
+    public float FlickerFor;
+    private float TimingValue = 0;
+    private float FlickerTime = 0;
+    private int OddOrEven = 0;
+
+    void Start()
+    {
+       if (speedMultiplier <= 0)
+        {
+            speedMultiplier = 1;
+        }
+        FlickerFor += timeBeforeFlicker;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        TimingValue += Time.deltaTime;
+        if (TimingValue > timeBeforeFlicker) 
+        {
+            if (TimingValue < FlickerFor)
+            {
+                FlickerTime += (Time.deltaTime * speedMultiplier);
+                OddOrEven = Mathf.FloorToInt(FlickerTime % 2);
+                if (OddOrEven == 1) // that's odd XD get it (I lame af sia lol)
+                {
+                    FlickerObject.SetActive(false);
+                }
+                if (OddOrEven == 0)
+                {
+                    FlickerObject.SetActive(true);
+                }
+            }
+            else
+            {
+                FlickerTime = 0;
+                TimingValue = 0;
+            }
+        }
+    }
+}
